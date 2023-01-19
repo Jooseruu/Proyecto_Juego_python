f73fe504dd03d082ed81cca89b108cff23cb5a77
Jooseeruu <arjona2908@gmail.com>
2023-01-19 17:38:29 +0100

    Primera version personajes

--- parteruben.py
+++ parteruben.py
@@ -1,2 +1,15 @@
-def table123():
-    print("prueba123")
+import random
+
+# Creamos una lista de personajes famosos
+Personajes = [
+    {"name": "Michael Jackson", "age": 50, "height": 175, "hair_color": "black"},
+    {"name": "Albert Einstein", "age": 76, "height": 160, "hair_color": "white"},
+    {"name": "Nelson Mandela", "age": 95, "height": 183, "hair_color": "black"},
+    {"name": "Princesa Diana", "age": 36, "height": 170, "hair_color": "blond"},
+    {"name": "Steve Jobs", "age": 56, "height": 168, "hair_color": "black"}
+    {"name": "Cristiano Ronaldo", "age": 37, "height": 187, "hair_color": "black"},
+    {"name": "Bad Bunny", "age": 28, "height": 180, "hair_color": "black"},
+    {"name": "Ronnie Coleman", "age": 58, "height": 180, "hair_color": "bald"},
+    {"name": "Jabib Nurmagomédov", "age": 34, "height": 178, "hair_color": "black"},
+    {"name": "Anuel AA", "age": 30, "height": 172, "hair_color": "black"}
+]
