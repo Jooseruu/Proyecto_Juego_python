1fc7865922c264444581d85b9eae63380dd1123c
Jooseeruu <arjona2908@gmail.com>
2023-01-22 19:48:42 +0100

    Segunda Version Personajes

--- parteruben.py
+++ parteruben.py
@@ -2,14 +2,19 @@ import random
 
 # Creamos una lista de personajes famosos
 Personajes = [
-    {"name": "Michael Jackson", "age": 50, "height": 175, "hair_color": "black"},
-    {"name": "Albert Einstein", "age": 76, "height": 160, "hair_color": "white"},
-    {"name": "Nelson Mandela", "age": 95, "height": 183, "hair_color": "black"},
-    {"name": "Princesa Diana", "age": 36, "height": 170, "hair_color": "blond"},
-    {"name": "Steve Jobs", "age": 56, "height": 168, "hair_color": "black"}
-    {"name": "Cristiano Ronaldo", "age": 37, "height": 187, "hair_color": "black"},
-    {"name": "Bad Bunny", "age": 28, "height": 180, "hair_color": "black"},
-    {"name": "Ronnie Coleman", "age": 58, "height": 180, "hair_color": "bald"},
-    {"name": "Jabib Nurmagomédov", "age": 34, "height": 178, "hair_color": "black"},
-    {"name": "Anuel AA", "age": 30, "height": 172, "hair_color": "black"}
+    {"name": "Michael Jackson", "age": 50, "height": 175, "gender": "Male", "hair_color": "black"},
+    {"name": "Albert Einstein", "age": 76, "height": 160, "gender": "Male", "hair_color": "white"},
+    {"name": "Nicki Nicole", "age": 22, "height": 145, "gender": "Female", "hair_color": "black"},
+    {"name": "Princesa Diana", "age": 36, "height": 170, "gender": "Female", "hair_color": "blond"},
+    {"name": "Steve Jobs", "age": 56, "height": 168, "gender": "Male", "hair_color": "black"}
+    {"name": "Cristiano Ronaldo", "age": 37, "height": 187, "gender": "Male", "hair_color": "black"},
+    {"name": "Bad Bunny", "age": 28, "height": 180, "gender": "Male", "hair_color": "black"},
+    {"name": "Ronnie Coleman", "age": 58, "height": 180, "gender": "Male", "hair_color": "bald"},
+    {"name": "Reina Letizia", "age": 50, "height": 170, "gender": "Female", "hair_color": "brown"},
+    {"name": "Aitana", "age": 23, "height": 160, "gender": "Female", "hair_color": "black"}
+    {"name": "Dwayne Douglas Johnson", "age": 50, "height": 196, "gender": "Male", "hair_color": "bald"},
+    {"name": "Vin Diesel", "age": 55, "height": 182, "gender": "Male", "hair_color": "bald"},
+    {"name": "Lionel Messi", "age": 35, "height": 169, "gender": "Male", "hair_color": "black"},
+    {"name": "Leonardo DiCaprio", "age": 48, "height": 183, "gender": "Male", "hair_color": "blond"},
+    {"name": "Shakira", "age": 45, "height": 157, "gender": "Female", "hair_color": "blond"}
 ]
