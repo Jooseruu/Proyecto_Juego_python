321e6c56505d3e8cf229c4445722ce0570ecec19
José Rubén Arjona <120745808+Jooseruu@users.noreply.github.com>
2023-01-22 23:55:19 +0100

    Versión Final Personajes

--- Personajes.py
+++ Personajes.py
@@ -11,7 +11,7 @@ Characters = [
     ["Bad Bunny",  28, 180, "Male", "earrings", "black"],
     ["Ronnie Coleman", 58, 180, "Male", "none", "bald"],
     ["Reina Letizia", 50, 170, "Female", "hat", "brown"],
-    ["Aitana", 23, 160, "Female","earrings", "black"]
+    ["Aitana", 23, 160, "Female","earrings", "black"],
     ["Dwayne Douglas Johnson", 50, 196, "Male", "none", "bald"],
     ["Vin Diesel", 55, 182, "Male", "none", "bald"],
     ["Lionel Messi",  35, 169, "Male", "none",  "black"],
@@ -22,7 +22,7 @@ Characters = [
 
 Personajes = [
     ["Michael Jackson",  50, 175, "Hombre", "no", "negro"],
-    ["Albert Einstein", 76, 160, "Hombre", "gafas", "blaco"],
+    ["Albert Einstein", 76, 160, "Hombre", "gafas", "blanco"],
     ["Nicki Nicole", 22, 145, "Mujer", "sombrero/gorro", "negro"],
     ["Princesa Diana", 36, 170, "Mujer", "pendientes", "rubio"],
     ["Steve Jobs", 56, 168, "Hombre", "gafas", "negro"],
@@ -30,7 +30,7 @@ Personajes = [
     ["Bad Bunny",  28, 180, "Hombre", "pendientes", "negro"],
     ["Ronnie Coleman", 58, 180, "Hombre", "no", "calvo"],
     ["Reina Letizia", 50, 170, "Mujer", "sombrero/gorro", "castaño"],
-    ["Aitana", 23, 160, "Mujer","pendientes", "negro"]
+    ["Aitana", 23, 160, "Mujer","pendientes", "negro"],
     ["Dwayne Douglas Johnson", 50, 196, "Hombre", "no", "calvo"],
     ["Vin Diesel", 55, 182, "Hombre", "no", "calvo"],
     ["Lionel Messi",  35, 169, "Hombre", "no",  "negro"],
