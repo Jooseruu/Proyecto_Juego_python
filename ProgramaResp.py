52d17b74597332b60d3b2495356c0f154d3cadf4
Jooseeruu <arjona2908@gmail.com>
2023-01-22 22:12:22 +0100

    Selección de nombre

---
+++ ProgramaResp.py
@@ -0,0 +1,7 @@
+print("-------------------------------")
+print("Bienvenido a Quien es Quien!")
+print("-------------------------------")
+ 
+ 
+nombre_jugador=input("¿Cual es tu nombre?" )
+print("Vamos a empezar a jugar", nombre_jugador, "buena suerte <3." )
