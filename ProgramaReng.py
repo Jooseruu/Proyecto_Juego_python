213bae4f3c826a7cc8cff9aa4367fcc00e0d9f74
José Rubén Arjona <120745808+Jooseruu@users.noreply.github.com>
2023-01-22 23:50:04 +0100

    Delete ProgramaReng.py

--- ProgramaReng.py
+++
@@ -1,7 +0,0 @@
-print("-------------------------------")
-print("Welcome to Guess Who!")
-print("-------------------------------")
- 
- 
-player_name=input("What is your name?" )
-print("Okey, let us start playing", player_name, ", good luck <3." )
