7f49149dfa67a8745422f1e8695653d3dcaed7ce
Jooseeruu <arjona2908@gmail.com>
2023-01-23 02:22:27 +0100

    Posible versión final de solamente el juego añadimos timer añadimos contador de intentos y añadimos while para poder volver a jugar

--- ProgramaResp.py
+++ ProgramaResp.py
@@ -1,4 +1,5 @@
 import random
+import time
 Personajes = [["Michael Jackson",  50, 175, "Hombre", "no", "negro"],
               ["Albert Einstein", 76, 160, "Hombre", "gafas", "blanco"],
               ["Nicki Nicole", 22, 145, "Mujer", "sombrero/gorro", "negro"],
@@ -15,15 +16,16 @@ Personajes = [["Michael Jackson",  50, 175, "Hombre", "no", "negro"],
               ["Leonardo DiCaprio", 48, 183, "Hombre", "no", "rubio"],
               ["Shakira", 45, 157, "Mujer", "pendientes", "rubio"]]
 
-print("-------------------------------")
-print("Bienvenido a Quien es Quien!")
-print("-------------------------------")
+print("*******************************")
+print("|Cargando.....|")
+print("*******************************")
+
+time.sleep(3)
  
  
 nombre_jugador=input("¿Cual es tu nombre?" )
-print("Vamos a empezar a jugar", nombre_jugador, ", buena suerte <3." )
+print("Comenzamos a jugar", nombre_jugador, ", buena suerte <3." )
 
-Personaje= ["Vin Diesel", 55, 182, "Hombre", "no", "calvo"]
 Personaje= random.choice(Personajes)
 nombre= Personaje[0]
 edad= Personaje[1]
@@ -32,56 +34,68 @@ genero= Personaje[3]
 accesorios= Personaje[4]
 Colordepelo= Personaje[5]
 
-print("El nombre es:", nombre)
-print("La edad es:", edad)
-print("La altura en cm es:", altura)
-print("Es:", genero)
-print("Usa algun accesorio:", accesorios)
-print("Su pelo es:", Colordepelo)
+Número_intentos=0
+comienzo_tiempo = time.perf_counter()
+while True:
 
-adivina_edad=int(input("¿Que edad piensas que tiene?"))
-if adivina_edad==edad:
+ adivina_edad=int(input("¿Que edad piensas que tiene?"))
+ time.sleep(2)
+ if adivina_edad==edad:
     print("Sí, correcto!")
-else:
+ else:
     if edad<adivina_edad:
         print("Ala!, no es tan viejo mi personaje")
     else:
         print("Es de una edad mas avanzada que la que me has propuesto")
 
-adivina_altura= int(input("¿Que altura tiene en cm?"))
-if adivina_altura==altura:
+ adivina_altura= int(input("¿Que altura tiene mi personaje en cm?"))
+ time.sleep(2)
+ if adivina_altura==altura:
     print("Sí, correcto!")
-else:
+ else:
     if altura<adivina_altura:
         print("Es mas bajito el personaje escogido por mi ;)")
 
     else: 
         print("Es más alto mi personaje")
 
-adivina_genero= input("¿Que genero es?")
-adivina_genero= adivina_genero.title()
-if adivina_genero==genero:
+ adivina_genero= input("¿Que genero es?")
+ adivina_genero= adivina_genero.title()
+ time.sleep(2)
+ if adivina_genero==genero:
     print("Claro que sí!", nombre_jugador)
-else:
+ else:
     print("No, es", genero)
 
-adivina_accesorios= input("¿Que accesorios tiene si es que los tiene?")
-adivina_accesorios=adivina_accesorios.lower()
-if adivina_accesorios==accesorios:
+ adivina_accesorios= input("¿Que accesorios tiene si es que los tiene?")
+ adivina_accesorios=adivina_accesorios.lower()
+ time.sleep(2)
+ if adivina_accesorios==accesorios:
     print("Sí, correcto como siempre!")
-else:
+ else:
     print("No, te has equivocado", nombre_jugador)
 
-adivina_Colordepelo= input("¿Que Color de pelo tiene?")
-adivina_Colordepelo=adivina_Colordepelo.lower()
-if adivina_Colordepelo==Colordepelo:
+ adivina_Colordepelo= input("¿Que Color de pelo tiene?")
+ adivina_Colordepelo=adivina_Colordepelo.lower()
+ time.sleep(2)
+ if adivina_Colordepelo==Colordepelo:
     print("Sí, correctísimo!")
-else:
+ else:
     print("No, te has equivocado, más suerte la proxima", nombre_jugador)
 
-adivina= input("¿Quien piensas que es? ")
-adivina= adivina.title()
-if adivina==nombre:
+ adivina= input("¿Quien piensas que es? ")
+ adivina= adivina.title()
+ time.sleep(2)
+ if adivina==nombre:
     print("Felicidades has acertado!!!")
-else:
-    print("Más suerte la próxima, se trataba de:" + nombre)
+ else:
+    Número_intentos += 1
+    Resultado= input("No!!!,¿Desea volver a intentarlo, seguirá siendo el mismo personaje?(s/n)")
+    if Resultado =="n":
+     print("Más suerte la próxima, se trataba de:" + nombre)
+     final_tiempo = time.perf_counter()
+     durante_tiempo = final_tiempo - comienzo_tiempo
+     time.sleep(2)
+     print("El número de intentos realizado para adivinar el personaje ha sido de",Número_intentos)
+     print("Tiempo de juego: {:.2f} segundos.".format(durante_tiempo))
+     break
